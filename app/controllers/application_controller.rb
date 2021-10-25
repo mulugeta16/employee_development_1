cb9ab24a3e5a710376cd78b87d349991fe4130f8
mulugeta16 <mulugeta2030@gmail.com>
2021-10-25 15:05:43 +0900

    task model creation and validation completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  def create
+    @task = Task.new(task_params)
+    @task.user_id = current_user.id
+ if params[:back]
+  render :new
+  else
+    if @task.save
+    redirect_to tasks_path, notice: "The task was successfully created"
+      else
+         render :new
+    end
+  end
 end
